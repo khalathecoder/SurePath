95eb6358063bce83595d34e32cbd3026b286cc7c
Khala <khalathecoder@gmail.com>
2026-03-01 14:03:28 -0500

    Add project files.

---
+++ SurePath.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SurePath.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SurePath.Markets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SurePath.Markets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
